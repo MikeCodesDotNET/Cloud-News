07f648ecc938e55a417de6e6266933ee2157c137
Mike James <mijam@microsoft.com>
2019-08-09 11:43:44 +0100

    Adding in app notifiations from 3rd party code.
    Signed-off-by: Mike James <mijam@microsoft.com>

--- src/Advocates/Views/Notifications/HDNotificationData.swift
+++ src/Advocates/Views/Notifications/HDNotificationData.swift
@@ -1,9 +1,31 @@
 //
 //  HDNotificationData.swift
-//  Advocates
+//  HDNotificationView
 //
-//  Created by Mike James on 09/08/2019.
-//  Copyright © 2019 Mike James. All rights reserved.
+//  Created by nhdang103 on 10/6/18.
+//  Copyright © 2018 AnG Studio. All rights reserved.
 //
+//  From: https://github.com/nhdang103/HDNotificationView
 
 import Foundation
+import UIKit
+
+public class HDNotificationData: NSObject {
+    
+    public var iconImage: UIImage?
+    public var appTitle: String?
+    public var title: String?
+    public var message: String?
+    public var time: String?
+    
+    public init(iconImage: UIImage?, appTitle: String?, title: String?, message: String?, time: String?) {
+        
+        self.iconImage  = iconImage
+        self.appTitle   = appTitle
+        self.title      = title
+        self.message    = message
+        self.time       = time
+        
+        super.init()
+    }
+}
